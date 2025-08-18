66e43a011f34b4c77a3ebab1c0d134bf3b4f6bb4
unknown <marcosvincg@gmail.com>
2025-08-18 00:59:38 -0300

    tentando

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+// minha calculadora
+
+let numero1 = 80;
+let numero2 = 90;
+let sinal = "-";
+let resultado;
+if (sinal === "+") {
+    console.log(resultado = numero1 + numero2);
+}
+else if (sinal === "-") {
+    console.log(resultado = numero1 - numero2)
+}
+else if (sinal === "*") {
+    console.log(resultado = numero1 * numero2);
+}
+else if (sinal === "/") {
+    console.log(resultado = numero1 / numero2)
+}
+else {
+    console.log(resultado = "O seu resultado é inválido!")
+}
